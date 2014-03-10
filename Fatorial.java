b5252b697996ceb5105e0f6deccad8cdf72edb3e
Arthur Busqueiro <arthur.busqueiro@bcc.unifal-mg.edu.br>
2014-03-10 18:20:44 -0300

    Refatoracao da classe TesteFatorial e inclusao de comentarios em Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -1,10 +1,10 @@
 public class Fatorial {
+	//versao iterativa do calculo do fatorial
 	public int calcularFatorial(int n){
 		int resultado = 1;
 		
-		for (int i = 2; i <=n; i++){
+		for (int i = 2; i <=n; i++) 
 			resultado*= i;
-		}
-	return resultado;
+		return resultado;
 	}
 }

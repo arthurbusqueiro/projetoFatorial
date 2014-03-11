1e4daa2394c779199c8e6adef7a3a7e99c0e6515
Arthur Busqueiro <arthur.busqueiro@bcc.unifal-mg.edu.br>
2014-03-11 14:23:38 -0300

    Calculo do fatorial de 4 em vez de 0

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,15 +2,8 @@ class TesteFatorial {
 	//Comentario inserido em TesteFatorial.java
 	public static void main(String[] args){
 		Fatorial fat = new Fatorial();
-<<<<<<< HEAD
 		int resultado = fat.calcularFatorial(4);
 		System.out.println(resultado);	
-		resultado = fat.calcularFatorial(0);
-		System.out.println(resultado);	
-=======
-		int resultado = fat.calcularFatorial(0);
-		System.out.println(resultado);		
->>>>>>> calculo_fatorial_zero
 	}
 
 }
